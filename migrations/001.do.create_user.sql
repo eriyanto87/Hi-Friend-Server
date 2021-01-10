a5c4fa538d05398538ebb2d676e58d879f2f40fc
Evisari Riyanto <evisari111012@gmail.com>
2021-01-09 21:10:15 -0800

    connection

--- migrations/001.do.create_user.sql
+++ migrations/001.do.create_user.sql
@@ -4,4 +4,4 @@ CREATE TABLE "user" (
     "password" TEXT NOT NULL, 
     "first_name" TEXT NOT NULL, 
     "last_name" TEXT NOT NULL
-); .
+); 
--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN || "http://localhost:3000",
+    origin: process.env.CLIENT_ORIGIN,
   })
 );
 
